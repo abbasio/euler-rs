98231817048bcf7443d630ab2edee267d8944283
= <omarabbasi16@gmail.com>
2024-09-23 10:03:56 -0400

    generated function template from problem title

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,21 @@
+use clap::Parser;
 use codegen::{Scope, Function};
 use scraper::{Html, Selector};
 
 use std::fs;
 
+#[derive(Parser)]
+struct Cli {
+    problem: i16,
+}
+
 fn main(){
-
-   // Codegen
-   //let mut scope = Scope::new();
-
-   //let mut function = Function::new("even_fibonacci_numbers");
-   //function
-   //    .ret("i32");
-   // 
-   // scope.push_fn(function);
-   // println!("{:?}", scope.to_string());
-    generate(1);
+    let args = Cli::parse();
+    generate(args.problem);
 }
 
 fn generate(p: i16) {
+    // HTML Get/Parse
     let html = reqwest::blocking::get(format!("https://projecteuler.net/problem={p}"))
         .unwrap()
         .text()
@@ -27,13 +25,30 @@ fn generate(p: i16) {
     let title_selector = Selector::parse("h2").expect("unable to find problem title");
     
     let description_selector = Selector::parse("span.tooltiptext_right").expect("unable to find problem description");
-    let content_selector = Selector::parse("div.problem_content>p").expect("unable to find problem content");
+    let content_selector = Selector::parse("div.problem_content").expect("unable to find problem content");
 
-    let h2 = document.select(&title_selector).map(|x| x.inner_html());
+    let mut h2 = document.select(&title_selector).map(|x| x.inner_html());
     let desc = document.select(&description_selector).map(|x| x.inner_html());
     let problem = document.select(&content_selector).map(|x| x.inner_html());
     
-    h2.for_each(|line| println!{"{}", line});
+    println!("Generated information for problem {p}:");
+    h2.clone().for_each(|line| println!{"{}", line});
     desc.for_each(|line| println!{"{}", line});
     problem.for_each(|line| println!{"{}", line});
+
+    // Codegen
+    let mut scope = Scope::new();
+    
+    let function_name: &str = &h2.next()
+        .expect("unable to find problem title")
+        .to_lowercase()
+        .replace(" ", "_");
+
+    let mut function = Function::new(&function_name);
+    function
+        .ret("u32");
+    
+    scope.push_fn(function);
+    println!("Generated function template for problem {p}:");
+    println!("{:?}", scope.to_string());
 }

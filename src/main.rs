ca0d6d5d102c562b14fcbe36b36e8def231e0560
= <omarabbasi16@gmail.com>
2024-09-24 10:48:35 -0400

    formatted problem content + refactor

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+use aho_corasick::AhoCorasick;
 use clap::Parser;
 use codegen::{Scope, Function};
 use html2text::from_read;
 use scraper::{Html, Selector};
 
-use std::fs;
+use std::error::Error;
 
 #[derive(Parser)]
 struct Cli {
@@ -16,34 +17,15 @@ fn main(){
 }
 
 fn generate(p: i16) {
-    // HTML Get/Parse
-    let html = reqwest::blocking::get(format!("https://projecteuler.net/problem={p}"))
-        .unwrap()
-        .text()
-        .unwrap();
+    let html = get_html(p);
+    let mut problem_strings = parse_html(&html).into_iter(); 
     
-    //let text = from_read(html.as_bytes(), 20);
-    //println!("{:?}", text);
-    let document = Html::parse_document(&html); 
-    let title_selector = Selector::parse("h2").expect("unable to find problem title");
-    
-    let description_selector = Selector::parse("span.tooltiptext_right").expect("unable to find problem description");
-    let content_selector = Selector::parse("div.problem_content").expect("unable to find problem content");
-
-    let mut h2 = document.select(&title_selector).map(|x| x.inner_html());
-    let desc = document.select(&description_selector).map(|x| x.inner_html());
-    let problem = document.select(&content_selector).map(|x| x.inner_html());
-    
-    println!("Generated information for problem {p}:");
-    h2.clone().for_each(|line| println!{"/* {} */", line});
-    desc.for_each(|line| println!{"/* {} */", from_read(line.as_bytes(), 100)});
-    problem.for_each(|line| println!{"/* {} */", from_read(line.replace("$", "").as_bytes(), 100)});
 
     // Codegen
     let mut scope = Scope::new();
     
-    let function_name: &str = &h2.next()
-        .expect("unable to find problem title")
+    let function_name = problem_strings.clone().nth(0)
+        .expect("Problem name not found")
         .to_lowercase()
         .replace(" ", "_");
 
@@ -54,4 +36,56 @@ fn generate(p: i16) {
     scope.push_fn(function);
     println!("Generated function template for problem {p}:");
     println!("{:?}", scope.to_string());
+
+    println!("Generated information for problem {p}:");
+    problem_strings.for_each(|string| {
+        println!{"/* {} */", string}
+    });
+
 }
+
+fn get_html(p: i16) -> String {
+    return reqwest::blocking::get(format!("https://projecteuler.net/problem={p}"))
+        .unwrap()
+        .text()
+        .unwrap();
+}
+
+fn parse_html(html: &str) -> Vec<String> {
+    let document = Html::parse_document(&html); 
+    
+    // Selectors
+    let title_selector = Selector::parse("h2").expect("unable to find problem title");
+    let description_selector = Selector::parse("span.tooltiptext_right").expect("unable to find problem description");
+    let content_selector = Selector::parse("div.problem_content").expect("unable to find problem content");
+
+    let problem_title = document.select(&title_selector).map(|x| x.inner_html());
+    let problem_description = document.select(&description_selector).map(|x| x.inner_html());
+    let problem_content = document.select(&content_selector).map(|x| x.inner_html());
+    
+    let mut problem_strings: Vec<String> = Vec::new();
+
+    problem_title.for_each(|line| {
+        problem_strings.push(line);
+    });
+    problem_description.for_each(|line| {
+        let formatted_line = from_read(line.as_bytes(), 100);
+        problem_strings.push(formatted_line);
+    });
+    problem_content.for_each(|line| {
+        let formatted_line = from_read(format_problem_content(line).as_bytes(), 100);
+        problem_strings.push(formatted_line);
+    });
+
+    return problem_strings;
+}
+
+fn format_problem_content(line: String) -> String {
+   let patterns = &["$", r"\dots", r"\mod", r"\equiv"];
+   let replace_with = &["", "...", "%", "==="];
+
+   let ac = AhoCorasick::new(patterns).unwrap();
+   return ac.replace_all(&line, replace_with);
+}
+
+

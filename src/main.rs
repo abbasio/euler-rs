2a5bc06342c1d9039b75d17c40defecf1cd2fe44
= <omarabbasi16@gmail.com>
2024-09-25 11:03:26 -0400

    formatted problem description and refactored html parse map

--- src/main.rs
+++ src/main.rs
@@ -19,26 +19,25 @@ fn main(){
 
 fn generate(p: i16) {
     let html: String = get_html(p);
-    let mut problem_strings = parse_html(&html).into_iter(); 
+    let problem_strings = parse_html(&html); 
     
     let file_name = format!("{:0>7}", format!("{}.rs", p));
     let path = &file_name;
+    
+    let function_name = &problem_strings[0]
+        .to_lowercase()
+        .replace(" ", "_");
+
     let mut output = File::create(path).expect("Failed to create a file at path");
-    problem_strings.clone().for_each(|string| {
+    problem_strings.into_iter().for_each(|string| {
         write!(output, "/* {} */\n\n", string).expect("Failed to write problem content to file");
     });
     
     // Codegen
     let mut scope = Scope::new();
-    let function_name = problem_strings.nth(0)
-        .expect("Problem name not found")
-        .to_lowercase()
-        .replace(" ", "_");
-
     let mut function = Function::new(&function_name);
     function
         .ret("u32");
-    
     scope.push_fn(function);
     println!("Generated function template for problem {p}:");
     
@@ -54,34 +53,25 @@ fn get_html(p: i16) -> String {
 
 fn parse_html(html: &str) -> Vec<String> {
     let document = Html::parse_document(&html); 
+    let mut problem_strings: Vec<String> = Vec::new();
     
     // Selectors
     let title_selector = Selector::parse("h2").expect("unable to find problem title");
     let description_selector = Selector::parse("span.tooltiptext_right").expect("unable to find problem description");
     let content_selector = Selector::parse("div.problem_content").expect("unable to find problem content");
 
-    let problem_title = document.select(&title_selector).map(|x| x.inner_html());
-    let problem_description = document.select(&description_selector).map(|x| x.inner_html());
-    let problem_content = document.select(&content_selector).map(|x| x.inner_html());
+    problem_strings.extend(document.select(&title_selector).map(|x| x.inner_html()));
+    problem_strings.extend(document.select(&description_selector).map(|x| {
+       return from_read(format_desc(x.inner_html()).as_bytes(), 100);
+    }));
+    problem_strings.extend(document.select(&content_selector).map(|x| {
+       return from_read(format_content(x.inner_html()).as_bytes(), 100);
+    }));
     
-    let mut problem_strings: Vec<String> = Vec::new();
-
-    problem_title.for_each(|line| {
-        problem_strings.push(line);
-    });
-    problem_description.for_each(|line| {
-        let formatted_line = from_read(line.as_bytes(), 100);
-        problem_strings.push(formatted_line);
-    });
-    problem_content.for_each(|line| {
-        let formatted_line = from_read(format_problem_content(line).as_bytes(), 100);
-        problem_strings.push(formatted_line);
-    });
-
     return problem_strings;
 }
 
-fn format_problem_content(line: String) -> String {
+fn format_content(line: String) -> String {
    let patterns = &["$", r"\dots", r"\mod", r"\equiv"];
    let replace_with = &["", "...", "%", "==="];
 
@@ -89,4 +79,10 @@ fn format_problem_content(line: String) -> String {
    return ac.replace_all(&line, replace_with);
 }
 
-
+fn format_desc(line: String) -> String {
+    return line.split(";")
+        .enumerate()
+        .filter_map(|(i, el)| (i != 1)
+        .then(|| el))
+        .collect::<String>();
+}

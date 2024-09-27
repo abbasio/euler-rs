61cbe33c9f193346173bf7ec703823aae8b8ace6
= <omarabbasi16@gmail.com>
2024-09-27 12:45:55 -0400

    update template + some restructuring

---
+++ problems/example.rs
@@ -0,0 +1,30 @@
+/* Example of a euler-rs generated problem file */
+
+// Problem #1: Multiples of 3 or 5
+
+/*
+Published on Friday, 5th October 2001, 06:00 pm
+Difficulty rating: 5%
+*/
+
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The
+sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
+fn multiples_of_3_or_5() -> String {
+    // Your code here
+
+    // Feel free to create and use helper functions,
+    // but make sure this function returns your answer
+
+    // Make sure your answer is returned as a string!
+    return 0.to_string();
+}
+
+fn main() {
+    let answer = multiples_of_3_or_5();
+    println!("{}", answer);
+}
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ use html2text::from_read;
 use scraper::{Html, Selector};
 
 use std::process::{Command, Stdio};
-use std::fs::{File, exists};
-use std::io::Write;
+use std::fs::{File, create_dir_all};
+use std::io::{ErrorKind, Write};
 
 #[derive(Parser)]
 struct Cli {
@@ -16,30 +16,30 @@ struct Cli {
 fn main(){
     let args = Cli::parse();
     let url = format!("https://projecteuler.net/problem={}", args.p);
+ 
+    create_dir_all("problems").expect("failed to create problem directory");
+    create_dir_all("solutions").expect("failed to create solutions directory");
     
-    let file_name = format!("{:0>7}", format!("{}.rs", args.p));
-    if exists(&file_name).expect("unable to check if file exists") {
-        // Compile the problem 
-        Command::new("rustc")
-            .arg(&file_name)
-            .status()
-            .unwrap();
-        // Run the problem, retrieve output
-        let mut run_problem = String::from("./");
-        run_problem.push_str(&file_name[..4]);
-        let output = Command::new(run_problem)
-            .stdout(Stdio::piped())
-            .output()
-            .unwrap();
-        
-        let answer = String::from_utf8(output.stdout).unwrap();
-        submit_answer(answer);
-    } else {
-        generate_problem_file(&url, &file_name);
+    let file_name = format!("{:0>4}", format!("{}", args.p));
+    let path = String::from("problems/") + &file_name + ".rs";
+    
+    let file = File::create_new(&path);
+    match file {
+        Ok(new_file) => {
+            generate_problem_file(args.p, &url, new_file);
+            return;
+        },
+        Err(ref e) => match e.kind() {
+            ErrorKind::AlreadyExists => {
+                let answer = compile_and_run(&file_name);
+                submit_answer(answer);
+            }, 
+            _ => panic!("Cannot read from file: {}, error: {}", path, e),
+        },
     }
 }
 
-fn generate_problem_file(url: &str, file_name: &str) {
+fn generate_problem_file(p: i16, url: &str, mut file: File) {
     let html: String = get_html(url);
     let problem_strings = parse_html(&html); 
     
@@ -47,15 +47,22 @@ fn generate_problem_file(url: &str, file_name: &str) {
         .to_lowercase()
         .replace(" ", "_");
 
-    let mut output = File::create(file_name).expect("Failed to create a file at path");
-    problem_strings.into_iter().for_each(|string| {
-        write!(output, "/*\n{}\n*/\n\n", string).expect("Failed to write problem content to file");
+    problem_strings
+        .into_iter()
+        .enumerate()
+        .for_each(|(i, mut string)| {
+        if i == 0 {
+            write!(file, "// Problem #{}: {}\n\n", p, string).expect("Failed to write problem title to file");
+        } else {
+            string.pop();
+            write!(file, "/*\n{}\n*/\n\n", string).expect("Failed to write problem content to file");
+        }
     });
     
     let code_template = generate_code_template(fn_name);
-    write!(output, "{}", code_template).expect("Failed to write function template to file");
+    write!(file, "{}", code_template).expect("Failed to write function template to file");
     
-    println!("Generated problem file {}", file_name);
+    println!("Generated problem file");
 }
 
 fn get_html(url: &str) -> String {
@@ -90,28 +97,30 @@ fn format_content(line: String) -> String {
    let replace_with = &["", "...", "%", "==="];
 
    let ac = AhoCorasick::new(patterns).unwrap();
-   let formatted_content = ac.replace_all(&line, replace_with);
-   
-   return from_read(formatted_content.as_bytes(), 100);
+   let content = ac.replace_all(&line, replace_with);
+   return from_read(content.as_bytes(), 100);
 }
 
 fn format_desc(line: String) -> String {
-    let filtered_desc = line.split(";")
+    let desc = line.split(";")
         .enumerate()
         .filter_map(|(i, el)| (i != 1)
         .then(|| el))
         .collect::<String>();
-    return from_read(filtered_desc.as_bytes(), 100);
+    return from_read(desc.as_bytes(), 100);
 }
 
 fn generate_code_template(fn_name: &str) -> String {
     let mut scope = Scope::new();
     
     let mut problem_fn = Function::new(fn_name);
-    problem_fn 
-        .line("// Your code here")
+    problem_fn
+        .ret("String")
+        .line("// Your code here\n")
         .line("// Feel free to create and use helper functions,")
-        .line("// but make sure this function returns your answer");
+        .line("// but make sure this function returns your answer\n")
+        .line("// Make sure your answer is returned as a string!")
+        .line("return 0.to_string();");
     
     let mut main_fn = Function::new("main");
     main_fn
@@ -122,6 +131,28 @@ fn generate_code_template(fn_name: &str) -> String {
     scope.push_fn(main_fn);
 
     return scope.to_string();
+}
+
+fn compile_and_run(file_name: &str) -> String {
+    let problem_path = String::from("./problems/") + file_name + ".rs"; 
+    let solution_path = problem_path
+        .replace("problems", "solutions")
+        .replace(".rs", "");
+
+    // Compile the problem 
+    Command::new("rustc")
+        .arg(&problem_path)
+        .args(["-o", &solution_path])
+        .status()
+        .unwrap();
+    
+    // Run the problem, retrieve output
+    let output = Command::new(solution_path)
+        .stdout(Stdio::piped())
+        .output()
+        .unwrap();
+        
+    return String::from_utf8(output.stdout).unwrap();
 }
 
 fn submit_answer(answer: String) {
